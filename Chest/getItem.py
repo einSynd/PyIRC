78af678d5de27a6926ba4eaaaf9d003d073f187e
einSynd <knivesu@gmail.com>
2017-05-21 17:16:55 -0700

    Added Caching As long as the program runs, items should be stored in a nested list so files don't have to be read after the first check for an item type.

--- Chest/getItem.py
+++ Chest/getItem.py
@@ -5,6 +5,9 @@ import re
 import random
 import csv
 
+itemDicts = {"Armor": {}, "Blue": {}, "Other": {}, "Healing": {},
+                "Spells": {}, "Weapon": {}, "Iris": {} }
+
 #List all of the CSV files by category, minus .csv
 fileDict = {"Armor": "Armor", "Blue": "Blue Treasure", "Other": "Other Usables",
             "Healing": "Restorative Usables", "Spells": "Spells", "Weapon": "Weapons",
@@ -28,80 +31,145 @@ def getFileName(type):
         return -1
     return fileName
 
-#Load weapon CSV, split into weapon name and chance name, and
-#   skip any weapons that start on floors after the chosen one
-def loadWeaponList(floorNum):
+#Receives weapon information and returns a list of weapons and chances based on floor number
+def pickFloorItem(floorNum, name, chance, floors):
+    availableNames = []
+    availableChances = []
+    excluded = 0
+    
+    for index in range(len(floors)):
+        if floors[index] != 0 and floors[index] <= floorNum:
+            availableNames.append(name[index])
+            availableChances.append(chance[index])
+        else:
+            excluded += 1
+    
+    return availableNames, availableChances
+
+#Load weapon CSV if not already loaded and split into weapon name, chance name, and floor number
+def getWeaponList():
     name = []
     chance = []
+    floors = []
+    
     type = "Weapon"
     
-    fileName = getFileName(type)
-    if type == -1:
-        return
-    
-    with open(fileName, "r") as csvfile:
-        fileReader = csv.reader(csvfile, delimiter=',', quotechar='|')
-        for row in fileReader:
-            startFloor = int(row[1])
-            if startFloor <= floorNum:
+    if "chance" in itemDicts[type]:
+        name = itemDicts[type]["name"]
+        chance = itemDicts[type]["chance"]
+        floors = itemDicts[type]["floors"]
+    else:
+        fileName = getFileName(type)
+        if type == -1:
+            return ["File not found"], [1]
+        
+        with open(fileName, "r") as csvfile:
+            fileReader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            for row in fileReader:
                 name.append(row[0])
+                floors.append(int(row[1]))
+                
                 tempChance = row[2]
                 chance.append(float(tempChance[:-1]))
+                
+        itemDicts[type]["name"] = name
+        itemDicts[type]["chance"] = chance
+        itemDicts[type]["floors"] = floors
     
-    return name, chance
+    return name, chance, floors
 
 #Load armor CSV, split into armor name and chance name, and
 #   skip any armors that start on floors after the chosen one.
 #   Extra difficulty added due to the floor 9 bug where most things
 #   stop showing up.
-def loadArmorList(floorNum):
+def getArmorList(floorNum):
     name = []
     chance = []
+    floors = []
+    
     type = "Armor"
     
-    fileName = getFileName(type)
-    if type == -1:
-        return
+    #Checked for arrays already loaded
+    if floorNum < 10 and "1-9" in itemDicts[type]:
+        name = itemDicts[type]["1-9"]["name"]
+        chance = itemDicts[type]["1-9"]["chance"]
+        floors = itemDicts[type]["1-9"]["floors"]
+    elif floorNum > 9 and "10+" in itemDicts[type]:
+        name = itemDicts[type]["10+"]["name"]
+        chance = itemDicts[type]["10+"]["chance"]
+        floors = itemDicts[type]["10+"]["floors"]
+    else:
+        fileName = getFileName(type)
+        if type == -1:
+            return ["File not found"], [1]
+        
+        #Armor CSV: name, start1, start2, chance
+        #   start1 is for floors 1-9, start2 is for floors 10-98; 
+        #   There's a bug where many armor types don't show up past floor 9.
+        #   start of 0 means not available
+        
+        with open(fileName, "r") as csvfile:
+            fileReader = csv.reader(csvfile, delimiter=",", quotechar="|")
+            for row in fileReader:
+                #Floors 1-9
+                if floorNum < 10:
+                    startFloor = int(row[1])
+                    #If it doesn't start on floor 0, then it's in this range
+                    if startFloor != 0:
+                        name.append(row[0])
+                        floors.append(startFloor)
+                        tempChance = row[3]
+                        chance.append(float(tempChance[:-1]))
+                #Floors 10 or lower
+                else:
+                    startFloor = int(row[2])
+                    #If it doesn't start on floor 0, then it's in this range
+                    if startFloor != 0 :
+                        name.append(row[0])
+                        floors.append(startFloor)
+                        tempChance = row[3]
+                        chance.append(float(tempChance[:-1]))
+        
+        if floorNum < 10:
+            itemDicts[type]["1-9"] = {}
+            itemDicts[type]["1-9"]["name"] = name
+            itemDicts[type]["1-9"]["chance"] = chance
+            itemDicts[type]["1-9"]["floors"] = floors
+        else:
+            itemDicts[type]["10+"] = {}
+            itemDicts[type]["10+"]["name"] = name
+            itemDicts[type]["10+"]["chance"] = chance
+            itemDicts[type]["10+"]["floors"] = floors
     
-    #Armor CSV: name, start1, end1, start2, end2, chance
-    #   start1/end1 are for floors 1-9, start2-end2 are for floors 10-98;
-    #   There's a bug where many armor types don't show up past floor 9.
-    with open(fileName, "r") as csvfile:
-        fileReader = csv.reader(csvfile, delimiter=",", quotechar="|")
-        for row in fileReader:
-            #Check for floor 9 or higher only equips
-            if floorNum <= 9:
-                startFloor = int(row[1])
-                if startFloor != 0 and floorNum >= startFloor:
-                    name.append(row[0])
-                    tempChance = row[5]
-                    chance.append(float(tempChance[:-1]))
-            #Floors 10 or lower
-            else:
-                startFloor = int(row[3])
-                if startFloor != 0 and floorNum >= startFloor:
-                    name.append(row[0])
-                    tempChance = row[5]
-                    chance.append(float(tempChance[:-1]))
-    
-    return name, chance
+    return name, chance, floors
 
 #Load any of the other CSVs, split into weapon name and chance name.
 #   These can be found on any floor.
-def loadBasicList(type):
+def getBasicList(type):
     name = []
     chance = []
     
-    fileName = getFileName(type)
-    if type == -1:
-        return
-    
-    with open(fileName, "r") as csvfile:
-        fileReader = csv.reader(csvfile, delimiter=',', quotechar='|')
-        for row in fileReader:
-            name.append(row[0])
-            tempChance = row[1]
-            chance.append(float(tempChance[:-1]))
+    #Check if the dict for that item type has stats already loaded
+    if "chance" in itemDicts[type]:
+        print("Item type %s is cached." % (type))
+        name = itemDicts[type]["name"]
+        chance = itemDicts[type]["chance"]
+    else:
+        #No stats, so load the CSV
+        fileName = getFileName(type)
+        
+        if type == -1:
+            return ["File not found"], [1]
+            
+        with open(fileName, "r") as csvfile:
+            fileReader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            for row in fileReader:
+                name.append(row[0])
+                tempChance = row[1]
+                chance.append(float(tempChance[:-1]))
+        
+        itemDicts[type]["chance"] = chance
+        itemDicts[type]["name"] = name
     
     return name, chance
 
@@ -120,21 +188,24 @@ def pickAnItem(forcedCat = "None"):
     
     #Load the arrays from file
     if pickedCat == "Armor":
-        pickedNames, pickedChances = loadArmorList(floorNum)
+        name, chance, floors = getArmorList(floorNum)
+        pickedNames, pickedChances = pickFloorItem(floorNum, name, chance, floors)
     elif pickedCat == "Weapon":
-        pickedNames, pickedChances = loadWeaponList(floorNum)
+        name, chance, floors = getWeaponList()
+        pickedNames, pickedChances = pickFloorItem(floorNum, name, chance, floors)
     else:
-        pickedNames, pickedChances = loadBasicList(pickedCat)
+        pickedNames, pickedChances = getBasicList(pickedCat)
     
     #Use name and chance arrays to pick a weighted item
     chosenDrop = random.choices(pickedNames, pickedChances)[0]
-    #Print drop's name
+    
+    #Return drop's information
     return floorNum, floorInfo, chosenDrop
 
 if __name__ == "__main__":
     forcedCat = "Armor"
-    print(pickAnItem())
-    print(pickAnItem())
-    print(pickAnItem())
-    print(pickAnItem())
-    print(pickAnItem())
+    print(pickAnItem(forcedCat))
+    print(pickAnItem(forcedCat))
+    print(pickAnItem(forcedCat))
+    print(pickAnItem(forcedCat))
+    print(pickAnItem(forcedCat))
